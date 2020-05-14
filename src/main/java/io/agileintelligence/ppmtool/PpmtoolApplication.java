96b797e49fbb3c5bb2dafd82b4d757ef2c752ecf
john-ngarambe <51552292+john-ngarambe@users.noreply.github.com>
2020-05-14 01:26:46 -0400

    update

--- src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
+++ src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PpmtoolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PpmtoolApplication.class, args);
 	}
 
 }
